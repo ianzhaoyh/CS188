8c0a97f8d7c97f666fbc630dd3202b4638307d55
ianzhaoyh <1758534277@qq.com>
2025-04-12 19:54:41 +0800

    half-done

--- project2/multiagent/multiAgents.py
+++ project2/multiagent/multiAgents.py
@@ -68,11 +68,39 @@ class ReflexAgent(Agent):
         to create a masterful evaluation function.
         """
         # Useful information you can extract from a GameState (pacman.py)
+        #远离鬼 + 减少食物 + 减少与食物的最短距离
         successorGameState = currentGameState.generatePacmanSuccessor(action)
-        newPos = successorGameState.getPacmanPosition()
-        newFood = successorGameState.getFood()
+        newPos = successorGameState.getPacmanPosition() #pacman移动之后的位置
+        newFood = successorGameState.getFood() #剩余的食物
         newGhostStates = successorGameState.getGhostStates()
-        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates] #示每个幽灵（Ghost）在 Pacman 吃下能量豆（Power Pellet）后，仍然处于“害怕状态”（Scared）的剩余步数
+        
+        score=0
+        # Food estimation
+        Food=newFood.asList()
+
+        min_dist=999
+        for food in Food:
+            dist=util.manhattanDistance(newPos,food)
+            if dist < min_dist:
+                score+=1/(dist+1)  #距离最近的食物越近越好
+
+        #Ghost_estimation
+        for index, GhostState in enumerate(newGhostStates):
+             ghostPos=GhostState.getPosition()
+             distance = util.manhattanDistance(newPos,ghostPos) 
+             if newScaredTimes[index]>0:
+                 score+=2/(distance+1)
+             else:
+                 if distance<4: #保证不死 （一个很大的penalty）
+                    score-=500
+                 else:
+                     score-=2/(distance+1)
+                 
+        
+        score+=successorGameState.getScore()
+
+        return score
 
         "*** YOUR CODE HERE ***"
         return successorGameState.getScore()
@@ -107,11 +135,47 @@ class MultiAgentSearchAgent(Agent):
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
 
+
 class MinimaxAgent(MultiAgentSearchAgent):
     """
     Your minimax agent (question 2)
     """
+    def minimax(self,state,depth,agentIndex):
+        if depth==0 or state.isWin() or state.isLose():
+            return self.evaluationFunction(state),None
+        legal_action=state.getLegalActions(agentIndex)
 
+        if not legal_action:
+            return self.evaluationFunction(state),None
+        
+        nextAgent=(agentIndex+1)%state.getNumAgents()
+        if nextAgent==0:
+            nextDepth=depth-1
+        else:
+            nextDepth=depth
+        #pacman:
+        if agentIndex==0:
+            bestvalue=-999
+            bestaction=None
+            for action in legal_action:
+                successor=state.generateSuccessor(agentIndex,action)
+                value,_=self.minimax(successor,nextDepth,nextAgent)
+                if value>bestvalue:
+                    bestvalue=value
+                    bestaction=action
+            return bestvalue,bestaction
+        else: # ghost
+            worstvalue=1000
+            worstaction=None
+            for action in legal_action:
+                successor=state.generateSuccessor(agentIndex,action)
+                value,_=self.minimax(successor,nextDepth,nextAgent)
+                
+                if value<worstvalue:
+                    worstvalue=value
+                    worstaction=action
+            return worstvalue,worstaction
+                
     def getAction(self, gameState: GameState):
         """
         Returns the minimax action from the current gameState using self.depth
@@ -136,18 +200,76 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
+
+        _,action=self.minimax(gameState,self.depth,0)
+        return action
+        
         util.raiseNotDefined()
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
     Your minimax agent with alpha-beta pruning (question 3)
     """
-
+      
     def getAction(self, gameState: GameState):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
+
+        def max_value(state,a,b,agentIndex,depth):
+         v=-9999
+         best_action=None
+         legal_action=state.getLegalActions(agentIndex)
+         agent=(agentIndex+1)%state.getNumAgents()
+         if agent==0:
+             nextDepth=depth-1
+         else:
+            nextDepth=depth
+         for action in legal_action:
+            v=max(v,value(state.generateSuccessor(agentIndex,action),a,b,agent,nextDepth))
+            if v>=b:
+                return v #pruning
+            a=max(a,v)
+         return v
+        
+        def min_value(state,a,b,agentIndex,depth):
+         v=9999
+         legal_action=state.getLegalActions(agentIndex)
+         agent=(agentIndex+1)%state.getNumAgents()
+         if agent==0:
+            nextDepth=depth-1
+         else:
+            nextDepth=depth
+         for action in legal_action:
+            v=min(v,value(state.generateSuccessor(agentIndex,action),a,b,agent,nextDepth))
+            if v<=a: #min节点的value只会越来越小，如果当下已经小于max的最好值了，就可以直接返回出栈了
+                return v,  #pruning
+            b=min(b,v) 
+         return v 
+
+        def value(state,a,b,agent,depth):
+            if state.isWin() or state.isLose() or depth-1==0:
+                return self.evaluationFunction(state)
+            elif agent==0:
+                return max_value(state,a,b,agent,depth)
+            else:
+                return min_value(state,a,b,agent,depth)
+        
+        legalActions=gameState.getLegalActions(0)
+        a=-9990
+        b=9999
+        bs=-1000
+        ba=None
+        for action in legalActions:
+            score=value(gameState.generateSuccessor(0,action),a,b,1,self.depth)
+            if score>bs:
+                bs=score
+                ba=action
+
+            a=max(a,bs)
+        return ba
+
         util.raiseNotDefined()
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
